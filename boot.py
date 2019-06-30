9e1a20f951bf3be2af181eb92832d5cb5177da9e
Laydas <iain.letourneau@gmail.com>
2019-06-30 00:15:29 -0600

    Added funtion in readPot to return Pot value

--- boot.py
+++ boot.py
@@ -1,5 +1,8 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-# #import esp
-# #esp.osdebug(None)
-# import gc#import webrepl
-# #webrepl.start()\ngc.collect()
+# import esp
+# esp.osdebug(None)
+import gc
+# import webrepl
+# webrepl.start()
+gc.collect()
+import readPot
--- readPot.py
+++ readPot.py
@@ -1,1 +1,11 @@
 # This module reads the value from the Potentiometer on pin A0
+
+# Import neccessary modules to read from Analog Pin
+from machine import Pin, ADC
+
+# This is for testing only, remove this before merging with master
+from time import sleep
+
+# Constantly read the potentiometer and print out the values
+def getSetting():
+	return ADC(0).read()
